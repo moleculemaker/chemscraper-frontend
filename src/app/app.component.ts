dbc1795ef5bd737797a328f0eecb0476f46be082
Edwin Kuo <edwin@wirelessprv-10-192-151-24.near.illinois.edu>
2023-03-27 13:47:10 -0500

    change the user feedback email account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  emailstring: string = "mailto:moleculemaker@illinois.edu?Subject=User feedback for CLEAN";
+  emailstring: string = "mailto:clean-feedback@moleculemaker.org?Subject=User feedback for CLEAN";
   showCite: boolean = false;
   citeButton() {
     this.showCite = !this.showCite;
